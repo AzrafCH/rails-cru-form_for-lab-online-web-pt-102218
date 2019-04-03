1f41905f40ae2ef6665be598609889abcbd61962
Azraf Chowdhury <achowdhur@ltu.edu>
2019-04-03 19:14:44 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,36 @@
 class GenresController < ApplicationController
+
+  def index
+    @genres = Genre.all
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def create
+    @genre = Genre.new(genre_params(:name))
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params(:bio))
+    redirect_to artist_path(@artist)
+  end
+
+    private
+
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
+  end
 end
