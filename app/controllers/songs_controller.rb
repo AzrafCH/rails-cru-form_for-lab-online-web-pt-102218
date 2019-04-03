596f2be6c7b8fc8c31a4c7bfecf833fa2f60622c
Azraf Chowdhury <achowdhur@ltu.edu>
2019-04-03 19:17:12 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,36 @@
 class SongsController < ApplicationController
+
+  def index
+    @artists = Artist.all
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def create
+    @artist = Artist.new(artist_params(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params(:name, :artist_id, :genre_id))
+    redirect_to artist_path(@song)
+  end
+
+    private
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
 end
