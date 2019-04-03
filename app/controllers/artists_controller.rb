56f6d56ffa55dfc99da36fb732756f64ec23f3b8
Azraf Chowdhury <achowdhur@ltu.edu>
2019-04-03 19:00:29 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,6 +12,10 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
   end
 
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
   def create
     @artist = Artist.new(artist_params(:name, :bio))
     @artist.save
