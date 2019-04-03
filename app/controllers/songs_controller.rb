34af9aedc6d05be1ec4b459d4ee203321e9068c5
Azraf Chowdhury <achowdhur@ltu.edu>
2019-04-03 19:18:12 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,8 +17,8 @@ class SongsController < ApplicationController
   end
 
   def create
-    @artist = Artist.new(artist_params(:name, :bio))
-    @artist.save
+    @song = Song.new(song_params(:name, :artist_id, :genre_id))
+    @song.save
     redirect_to song_path(@song)
   end
 
