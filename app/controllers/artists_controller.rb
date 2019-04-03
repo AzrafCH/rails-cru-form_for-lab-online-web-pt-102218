45671b4f33cce045e2ecad369777cff6df653a18
Azraf Chowdhury <achowdhur@ltu.edu>
2019-04-03 18:58:10 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,4 +5,10 @@ class ArtistsController < ApplicationController
     @artist.save
     redirect_to artist_path(@artist)
   end
+
+  def update 
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params(:bio))
+    redirect_to artist_path(@artist)
+  end 
 end
